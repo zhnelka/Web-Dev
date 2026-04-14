c54b8bf58f36e0e20eb4afa2d52e1a4f6a323a51
zhanek <azimbai.zhaneka@gmail.com>
2026-04-15 00:39:31 +0500

    Add files via upload

---
+++ lab9/shop-back-drf/api/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import DefaultRouter
+from .views import CategoryViewSet, ProductViewSet
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'products', ProductViewSet, basename='product')
+
+urlpatterns = router.urls
